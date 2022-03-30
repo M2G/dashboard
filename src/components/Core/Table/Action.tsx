c41dea4d4b6348200d75092e7f44a9a5474a8052
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2022-03-30 03:18:07 +0200

    rm unused label

--- src/components/Core/Table/Action.tsx
+++ src/components/Core/Table/Action.tsx
@@ -4,10 +4,10 @@ import { string, func, arrayOf, shape } from 'prop-types';
 function Action({ actions }: any) {
   return (
     <div>
-      {actions.map(({ id, label, action, icon, iconType = 'fas' }: any) => (
+      {actions.map(({ id, action, icon, iconType = 'fas' }: any) => (
         <div key={`actionBarCol__${id}`} className="actionBar_button">
           <div id={id} onClick={action}>
-            <i className={`${iconType} ${icon}`}>{label}</i>
+            <i className={`${iconType} ${icon}`} />
           </div>
         </div>
       ))}
@@ -20,7 +20,6 @@ const actionType = shape({
   action: func.isRequired,
   icon: string.isRequired,
   iconType: string,
-  label: string.isRequired,
 });
 
 Action.propTypes = {
