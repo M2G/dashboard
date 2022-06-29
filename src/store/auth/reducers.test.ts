6bbdc7a6fc94fe2562f6323a4e1a256759dcf5df
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2022-06-29 03:12:09 +0200

    add first test reducer

--- src/store/auth/reducers.test.ts
+++ src/store/auth/reducers.test.ts
@@ -4,6 +4,31 @@ describe('todos reducer', () => {
   it('should handle initial state', () => {
    expect(
         authReducer(undefined, { data: [], errors: {}, type: '' }),
-     ).toEqual([]);
+     ).toEqual({ data: [], errors: undefined, loading: false});
+  });
+
+  it('should handle ADD_TODO', () => {
+    const data = [
+      {
+        _id: "62bb94deb0f09b1a1803ce3e",
+        created_at: "2022-06-28T23:55:10.120Z",
+        email: "foucauld@soundcast.fm",
+        first_name: "test",
+        last_name: "test",
+        modified_at: "2022-06-28T23:55:23.421Z",
+      },
+    ];
+
+    expect(
+      authReducer(
+{ data: [], errors: undefined, loading: false },
+        { data, type: 'AUTH_GET_USER_PROFIL_SUCCESS' },
+),
+)
+      .toEqual({
+        data: [],
+      errors: undefined,
+      loading: false,
+    });
   });
 });
