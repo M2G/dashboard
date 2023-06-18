3280ca9f8deeba2d9bf9ecea29ba4296282b5f29
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2023-06-19 00:11:46 +0200

    feat: refactor src/components part

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,7 @@
 /*eslint-disable*/
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
-import UserFilters from 'containers/UserFilters';
+import UserFilters from '../../containers2/UserFilters';
 
 function Navbar() {
   const { i18n } = useTranslation();
