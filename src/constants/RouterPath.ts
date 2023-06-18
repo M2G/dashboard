15951c5ff27bbcfdf3919392afa9c3242fd88da5
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2023-06-19 00:12:19 +0200

    feat: refactor  src/constants

--- src/constants/RouterPath.ts
+++ src/constants/RouterPath.ts
@@ -1,11 +1,14 @@
 const ROUTER_PATH = {
+  CHANGE_PASSWORD: '/change-password',
   DASHBOARD: '/dashboard',
   FORGOT_PASSWORD: '/forgot-password',
   HOME: '/',
   LOGIN: '/login',
+  PROFIL: '/profil',
   RESET_PASSWORD: '/reset-password',
   SIGNIN: '/signin',
   SIGNUP: '/signup',
+  USERS: '/users',
 };
 
 export default ROUTER_PATH;
