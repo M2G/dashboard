ac4b7238b45dbe6b725481f9827aab7a4e8f346b
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2023-06-19 17:33:09 +0200

    feat: refactor and use existing component form

--- src/containers2/Signin/Signin.tsx
+++ src/containers2/Signin/Signin.tsx
@@ -1,17 +1,23 @@
+import type { JSX } from 'react';
 import { useCallback } from 'react';
-import { useDispatch } from 'react-redux';
+import {connect, useDispatch} from 'react-redux';
 import { signinUserAction } from 'store/signin/actions';
-import SiginForm from 'components/SigninForm';
+import SigninForm from 'components/SigninForm';
 import { INITIAL_VALUES } from './constants';
 
-function Signin() {
+function Signin(): JSX.Element {
   const dispatch = useDispatch();
   const onSubmit = useCallback(
-      (e: any) => dispatch(signinUserAction(e)),
+      (e: {email: string; password: string }) => dispatch(signinUserAction(e)),
       [dispatch],
   );
 
-  return <SiginForm initialValues={INITIAL_VALUES} onSubmit={onSubmit} />;
+  return <SigninForm initialValues={INITIAL_VALUES} onSubmit={onSubmit} />;
 }
 
-export default Signin;
+const mapStateToProps = function(state) {
+  console.log('Signin Signin Signin state', state);
+  return {};
+};
+
+export default connect(mapStateToProps)(Signin);
