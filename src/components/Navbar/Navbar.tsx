c4c7c67219bb11cf21bb2d568a613c748e9d8c23
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2023-06-20 05:12:20 +0200

    feat: refactor

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,44 +1,41 @@
-/*eslint-disable*/
-import { useState } from "react";
-import { useTranslation } from "react-i18next";
-import UserFilters from '../../containers2/UserFilters';
+import type { JSX } from 'react';
 
-function Navbar() {
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+
+function Navbar(): JSX.Element {
   const { i18n } = useTranslation();
   const [state, setState] = useState<boolean>(false);
-
-  console.log('state', state)
   return (
-    <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-      <div className="container-fluid">
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarCollapse"
-          aria-controls="navbarCollapse"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-        <div className="collapse navbar-collapse" id="navbarCollapse">
-          <ul className="navbar-nav me-auto mb-2 mb-md-0">
-            <li className="nav-item">
-              <a href="/" className="nav-link active">
-                Home
-              </a>
-            </li>
-          </ul>
-          <button type="button" className="me-2 btn btn-light" onClick={() => {
-            setState(!state);
-            if (state) return i18n?.changeLanguage("fr");
-            i18n?.changeLanguage("en");
-          }}>{state ? 'fr' : 'en'}</button>
-          <UserFilters />
+      <nav className="navbar navbar-expand-md">
+        <div className="container-fluid">
+          <button
+              aria-controls="navbarCollapse"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+              className="navbar-toggler"
+              data-bs-target="#navbarCollapse"
+              data-bs-toggle="collapse"
+              type="button"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarCollapse">
+            <div className="navbar-nav me-auto mb-2 mb-md-0" />
+            <button
+                onClick={async () => {
+                  setState(!state);
+                  if (state) return i18n?.changeLanguage('fr');
+                  i18n?.changeLanguage('en');
+                }}
+                className="me-2 btn btn-light"
+                type="button"
+            >
+              {state ? 'fr' : 'en'}
+            </button>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
   );
 }
 
--- src/components/NoData/NoData.tsx
+++ src/components/NoData/NoData.tsx
@@ -1,3 +1,5 @@
+import type { JSX } from 'react';
+
 function NoData(): JSX.Element {
   return <div>No data</div>;
 }
