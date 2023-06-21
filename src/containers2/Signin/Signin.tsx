d13acc24fb0fd7a4c0f63408f6b3c2410018c14e
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2023-06-21 04:46:20 +0200

    feat: refactor signin part

--- src/containers2/Signin/Signin.tsx
+++ src/containers2/Signin/Signin.tsx
@@ -1,6 +1,6 @@
 import type { JSX } from 'react';
 import { useCallback } from 'react';
-import {connect, useDispatch} from 'react-redux';
+import {useDispatch} from 'react-redux';
 import { signinUserAction } from 'store/signin/actions';
 import SigninForm from 'components/SigninForm';
 import { INITIAL_VALUES } from './constants';
@@ -15,9 +15,4 @@ function Signin(): JSX.Element {
   return <SigninForm initialValues={INITIAL_VALUES} onSubmit={onSubmit} />;
 }
 
-const mapStateToProps = function(state) {
-  console.log('Signin Signin Signin state', state);
-  return {};
-};
-
-export default connect(mapStateToProps)(Signin);
+export default Signin;
