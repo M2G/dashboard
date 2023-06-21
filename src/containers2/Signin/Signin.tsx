99534c529537dafea9765b7dccd3d05d39d28c19
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2023-06-21 23:43:25 +0200

    feat: refactor src/containers2

--- src/containers2/Signin/Signin.tsx
+++ src/containers2/Signin/Signin.tsx
@@ -1,18 +1,34 @@
 import type { JSX } from 'react';
-import { useCallback } from 'react';
-import {useDispatch} from 'react-redux';
+import {useCallback, useContext, useEffect} from 'react';
+import {connect, useDispatch} from 'react-redux';
 import { signinUserAction } from 'store/signin/actions';
 import SigninForm from 'components/SigninForm';
 import { INITIAL_VALUES } from './constants';
-
-function Signin(): JSX.Element {
+import { AuthContext } from '../../AuthContext';
+function Signin({
+                  loading,
+                  signin,
+                }): JSX.Element {
+  const { activateAuth }: any = useContext(AuthContext);
   const dispatch = useDispatch();
   const onSubmit = useCallback(
       (e: {email: string; password: string }) => dispatch(signinUserAction(e)),
       [dispatch],
   );
 
+  useEffect(() => {
+    console.log('signin.token', signin?.token)
+    signin && activateAuth(signin.token);
+    }, [activateAuth, signin]);
+
   return <SigninForm initialValues={INITIAL_VALUES} onSubmit={onSubmit} />;
 }
 
-export default Signin;
+const mapStateToProps = (state: { signin: { data: any; loading: any; }; }) => {
+  return {
+    loading: state.signin.loading,
+    signin: state.signin.data,
+  };
+};
+
+export default connect(mapStateToProps)(Signin);
