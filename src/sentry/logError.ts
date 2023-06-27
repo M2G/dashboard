5d1fff14b8df21fb8396685ee6aab5c1ca1a9243
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2023-06-27 18:49:36 +0200

    feat: refactor sentry

--- src/sentry/logError.ts
+++ src/sentry/logError.ts
@@ -1,9 +1,14 @@
-import { withScope, captureException, Severity } from "@sentry/react";
-import NotFoundException from "exceptions/NotFoundException";
+import type { Scope } from '@sentry/react';
+import { withScope, captureException } from '@sentry/react';
+import NotFoundException from 'exceptions/NotFoundException';
+
+declare enum Severity {
+  Error = 'error'
+}
 
 export const logError = (error: Error): void => {
   if (!(error instanceof NotFoundException)) {
-    withScope((scope) => {
+    withScope((scope: Scope) => {
       scope.setLevel(Severity.Error);
       captureException(error);
     });
