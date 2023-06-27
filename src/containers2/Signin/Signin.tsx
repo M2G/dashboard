d1256234fe37b6205e3c8d24dd6d32e0e22f4e2b
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2023-06-27 18:49:12 +0200

    feat: add src/containers2/Signin

--- src/containers2/Signin/Signin.tsx
+++ src/containers2/Signin/Signin.tsx
@@ -1,35 +1,32 @@
 import type { JSX } from 'react';
-import {useCallback, useContext, useEffect} from 'react';
-import {connect, useDispatch} from 'react-redux';
+import { useCallback, useContext, useEffect } from 'react';
+import { connect, useDispatch } from 'react-redux';
 import { signinUserAction } from 'store/signin/actions';
 import SigninForm from 'components/SigninForm';
 import { INITIAL_VALUES } from './constants';
 import { AuthContext } from '../../AuthContext';
 
 interface ISignin {
-    loading: boolean;
-    signin: any;
+  loading: boolean;
+  signin: any;
 }
 
-function Signin({
-                  loading,
-                  signin,
-                }: ISignin): JSX.Element {
+function Signin({ loading, signin }: ISignin): JSX.Element {
   const { activateAuth }: any = useContext(AuthContext);
   const dispatch = useDispatch();
   const onSubmit = useCallback(
-      (e: {email: string; password: string }) => dispatch(signinUserAction(e)),
-      [dispatch],
+    (e: { email: string; password: string }) => dispatch(signinUserAction(e)),
+    [dispatch],
   );
 
   useEffect(() => {
-      (signin?.token && activateAuth(signin.token));
-    }, [activateAuth, signin?.token]);
+    signin?.token && activateAuth(signin.token);
+  }, [activateAuth, signin?.token]);
 
   return <SigninForm initialValues={INITIAL_VALUES} onSubmit={onSubmit} />;
 }
 
-const mapStateToProps = (state: { signin: { data: any; loading: any; }; }) => {
+const mapStateToProps = (state: { signin: { data: any; loading: any } }) => {
   return {
     loading: state.signin.loading,
     signin: state.signin.data,
