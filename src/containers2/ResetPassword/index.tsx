ae6fbf53365a5f7f1e5dcf052d7d8e29bb7e7245
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2023-06-29 00:01:15 +0200

    feat: add src/containers2/ResetPassword

--- src/containers2/ResetPassword/index.tsx
+++ src/containers2/ResetPassword/index.tsx
@@ -1,27 +1,8 @@
 import type { JSX } from 'react';
-import { useCallback } from 'react';
-import { useDispatch } from 'react-redux';
-import { useLocation } from 'react-router-dom';
-import { authRecoverPasswordAction } from 'store/auth/actions';
-import ResetPasswordView from 'components/ResetPasswordForm';
-import { INITIAL_VALUES } from './constants';
+import ResetPassword from './ResetPassword';
 
-function ResetPassword(): JSX.Element {
-  const { search } = useLocation();
-  const dispatch = useDispatch();
-
-  const onSubmit = useCallback(
-    (e) => {
-      const searchParams = new URLSearchParams(search);
-      if (searchParams.has('token')) {
-        const token = searchParams.get('token');
-        dispatch(authRecoverPasswordAction({ ...e, token }));
-      }
-    },
-    [dispatch, search],
-  );
-
-  return <ResetPasswordView initialValues={INITIAL_VALUES} onSubmit={onSubmit} />;
+function ResetPasswordPage(): JSX.Element {
+  return <ResetPassword />;
 }
 
-export default ResetPassword;
+export default ResetPasswordPage;
