8b34308d3e8cb284d8a071d621ee9d8174a6210e
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2023-07-02 05:12:13 +0200

    feat: refactor src/routes/PrivateRoutes

--- src/routes/PrivateRoutes/PrivateRoutes.tsx
+++ src/routes/PrivateRoutes/PrivateRoutes.tsx
@@ -1,21 +1,22 @@
 import type { JSX } from 'react';
+
 import { lazy } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import ROUTER_PATH from 'constants/RouterPath';
 
-const Home = lazy( () => import('../../containers/Home'));
-const Users = lazy( () => import('../../containers/Users'));
-const ChangePassword = lazy( () => import('../../containers/ChangePassword'));
-const Profil = lazy( () => import('../../containers/Profil'));
+const Home = lazy(() => import('../../containers/Home'));
+const Users = lazy(() => import('../../containers/Users'));
+const ChangePassword = lazy(() => import('../../containers/ChangePassword'));
+const Profil = lazy(() => import('../../containers/Profil'));
 
 function PrivateRoutes(): JSX.Element {
   return (
     <Routes>
-      <Route path={ROUTER_PATH.HOME} element={<Home />} />
-      <Route path={ROUTER_PATH.USERS} element={<Users />} />
-      <Route path={ROUTER_PATH.PROFIL} element={<Profil />} />
-      <Route path={ROUTER_PATH.CHANGE_PASSWORD} element={<ChangePassword />} />
-      <Route path="*" element={<Navigate to={ROUTER_PATH.HOME} replace />} />
+      <Route element={<Home />} path={ROUTER_PATH.HOME} />
+      <Route element={<Users />} path={ROUTER_PATH.USERS} />
+      <Route element={<Profil />} path={ROUTER_PATH.PROFIL} />
+      <Route element={<ChangePassword />} path={ROUTER_PATH.CHANGE_PASSWORD} />
+      <Route element={<Navigate replace to={ROUTER_PATH.HOME} />} path="*" />
     </Routes>
   );
 }
