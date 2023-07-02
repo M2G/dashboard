62ad9008689c1865198b1cf3b153d6ceb6db2e4a
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2023-07-02 05:11:01 +0200

    feat: add src/containers/Home

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,17 +1,5 @@
-import UserList from '../UserListItem/UserList';
-
 function Home() {
-  return <div className="o-zone">
-      <div className="o-grid">
-        <div className="o-grid__row">
-          <div className="o-col">
-            <div className="o-cell--one">
-              <UserList canEdit canDelete canAdd id="test" />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>;
+  return null;
 }
 
 export default Home;
--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -1,12 +1,18 @@
-/*eslint-disable*/
-import Navbar from 'components/Navbar/Navbar';
+import type { JSX } from 'react';
+
+import Navbar from 'components/Navbar';
+import Sidebar from 'components/Sidebar';
+
 import HomeView from './Home';
 
-function Home() {
-  return <>
-    <Navbar />
-    <HomeView />
-  </>
+function Home(): JSX.Element {
+  return (
+    <>
+      <Sidebar />
+      <Navbar />
+      <HomeView />
+    </>
+  );
 }
 
 export default Home;
