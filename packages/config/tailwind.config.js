c79ff8664d433135a55747ae75c213132e173e06
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2023-07-23 04:34:26 +0200

    feat: revert conf

--- packages/config/tailwind.config.js
+++ packages/config/tailwind.config.js
@@ -19,6 +19,41 @@ module.exports = {
       colors: {
         // Common
         ...defaultTheme.colors,
+        white: {
+          DEFAULT: "#FFFFFF",
+        },
+        black: {
+          DEFAULT: "#000000",
+        },
+        dark: {
+          DEFAULT: "#000000",
+          hover: "#3A3A3A",
+          active: "#181818",
+        },
+        grey: {
+          light: "#F5F5F5",
+          DEFAULT: "#EBEBEB",
+          dark: "#D6D6D6",
+        },
+        // Global
+        primary: {
+          // hover: "#111827",
+          DEFAULT: "#000000",
+          // active: "#D4314C",
+          // disabled: "#F69BA9",
+        },
+        secondary: {
+          hover: "#F5F5F5",
+          DEFAULT: "#EBEBEB",
+          active: "#D6D6D6",
+          disabled: "#F4F4F4",
+        },
+        // States
+        danger: {
+          DEFAULT: "#EB3F45",
+          disabled: "#F59FA2",
+          surface: "#FDECEC",
+        },
       },
     },
   },
