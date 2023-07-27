041398fe82b1659b775f68cc54d1148073888cde
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2023-07-27 02:15:29 +0200

    feat: update component ForgotPasswordForm

--- apps/web/src/components/ForgotPasswordForm/ForgotPasswordForm.tsx
+++ apps/web/src/components/ForgotPasswordForm/ForgotPasswordForm.tsx
@@ -1,20 +1,11 @@
 import type { SubmitHandler } from 'react-hook-form';
 import type { z } from 'zod';
 
-import ROUTER_PATH from '@/constants/RouterPath';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
-import { Link } from 'react-router-dom';
 
-import {
-  formSchema,
-  INPUT_NAME,
-  LABEL_EMAIL,
-  LABEL_PASSWORD,
-  PLACEHOLDER_EMAIL,
-  PLACEHOLDER_PASSWORD,
-} from './constants';
-import './index.scss';
+import { Button, Field } from 'ui';
+import { formSchema, INPUT_NAME, LABEL_EMAIL } from './constants';
 
 type FormSchemaType = z.infer<typeof formSchema>;
 
@@ -37,52 +28,24 @@ function ForgotPasswordForm({
   });
 
   return (
-    <div className="form-signup">
-      <form onSubmit={handleSubmit(onSubmit)}>
+    <div className="form-signup flex min-h-screen flex-col items-center justify-center">
+      <form className="rounded-2xl bg-white p-[25px]" onSubmit={handleSubmit(onSubmit)}>
         <div className="mb-4">
-          <h1 className="h3 mb-1">Please authenticate</h1>
+          <h1 className="h3 mb-1">Forgot password</h1>
           <span>to continue</span>
         </div>
-        <div className="form-floating">
-          <input
-            className="form-control mb-2"
-            id="floatingInput"
-            placeholder={PLACEHOLDER_EMAIL}
-            type="email"
-            {...register(INPUT_NAME.EMAIL)}
-            required
-          />
-          {errors?.[INPUT_NAME.EMAIL] ? (
-            <span className="error-text">{errors[INPUT_NAME.EMAIL].message}</span>
-          ) : null}
-          <label htmlFor="floatingInput">{LABEL_EMAIL}</label>
-        </div>
-        <div className="form-floating">
-          <input
-            className="form-control mb-2"
-            id="floatingPassword"
-            placeholder={PLACEHOLDER_PASSWORD}
-            type="password"
-            {...register(INPUT_NAME.PASSWORD)}
-            required
-          />
-          {errors?.[INPUT_NAME.PASSWORD] ? (
-            <span className="error-text">{errors[INPUT_NAME.PASSWORD].message}</span>
-          ) : null}
-          <label htmlFor="floatingPassword">{LABEL_PASSWORD}</label>
-        </div>
-        <button className="w-100 btn btn-lg btn-primary" disabled={isSubmitting} type="submit">
-          Sign up
-        </button>
-        <div className="c-action">
-          <span>Have an account ?</span>
-          <Link className="text-muted" to={ROUTER_PATH.SIGNIN}>
-            Signin
-          </Link>
-          <Link className="text-muted" to={ROUTER_PATH.FORGOT_PASSWORD}>
-            Forgot Password
-          </Link>
-        </div>
+        <Field
+          className="mb-2"
+          id="floatingInput"
+          label={LABEL_EMAIL}
+          // placeholder={PLACEHOLDER_EMAIL}
+          type="email"
+          {...register(INPUT_NAME.EMAIL)}
+          required
+        />
+        <Button className="w-full" disabled={isSubmitting} type="submit" variant="primary">
+          Submit
+        </Button>
       </form>
     </div>
   );
