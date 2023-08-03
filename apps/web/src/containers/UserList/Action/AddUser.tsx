f8354dc081c012e457a55bc7b2c81a7b0d060719
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2023-08-03 02:53:33 +0200

    feat: replace scss to tailwind

--- apps/web/src/containers/UserList/Action/AddUser.tsx
+++ apps/web/src/containers/UserList/Action/AddUser.tsx
@@ -8,9 +8,12 @@ interface IAddUser {
 function AddUser({ canAdd, onAdd }: IAddUser) {
   const { t } = useTranslation();
   return (
-    <div className="c-btn-user">
+    <div className="flex w-full justify-end">
       {canAdd && (
-        <button className="btn btn-light" type="submit" onClick={onAdd}>
+        <button
+          className="m-4 mb-2 rounded-md bg-gray-100 px-4 py-2 text-sm font-medium text-gray-800 hover:bg-gray-200"
+          type="submit"
+          onClick={onAdd}>
           {t('Add user')}
         </button>
       )}
