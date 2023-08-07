fa4cf689493cb1003610dfc09dd8c2adec389ca6
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2023-08-07 02:15:27 +0200

    feat: add conf tailwind

--- packages/ui/components/molecules/Field/Field.tsx
+++ packages/ui/components/molecules/Field/Field.tsx
@@ -67,11 +67,11 @@ export const Field: FC<FieldProps> = ({
       <div className="relative min-h-[52px] w-full">
         <DynamicTag
           className={[
-            'peer min-h-[52px] px-[15px] pt-4 font-sans text-sm font-normal outline outline-0',
-            'border-alt text-50 h-full w-full rounded-md border bg-white leading-6',
+            'peer min-h-[52px] px-[15px] pt-4 font-sans text-sm font-normal outline outline-0 dark:text-white',
+            'border-alt text-50 h-full w-full rounded-md border leading-6 dark:bg-[#121212]',
             'disabled:border-grey-dark disabled:bg-grey disabled:text-variants-50 disabled:cursor-not-allowed disabled:border',
             'placeholder-shown:border-alt',
-            'focus:border-black focus:outline-0',
+            'focus:border-black focus:outline-0 dark:border-[#3f3f46]',
             'transition-all',
             tag === 'textarea' && '_:min-h-[156px] _:pt-6',
             type === 'password' && '_:pr-16',
@@ -86,7 +86,7 @@ export const Field: FC<FieldProps> = ({
         <label
           className={[
             'pointer-events-none absolute left-4 top-2 flex h-full w-full select-none text-[12px] font-normal leading-[1.5] transition-all',
-            'text-gray-400 peer-placeholder-shown:top-0 peer-placeholder-shown:text-base peer-placeholder-shown:leading-[3.25] dark:text-gray-500',
+            'peer-placeholder-shown:top-0 peer-placeholder-shown:text-[14px] peer-placeholder-shown:leading-[3.5] dark:text-white',
             'peer-focus:top-2 peer-focus:text-[12px] peer-focus:leading-[1.5] peer-focus:after:scale-x-100',
             'peer-disabled:text-variants-50 peer-disabled:peer-placeholder-shown:text-variants-50',
           ].join(' ')}
