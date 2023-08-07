02900a5d89b265158c0cfa09cc2c9fcbbedfb163
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2023-08-08 01:46:25 +0200

    feat: use component field

--- apps/web/src/components/ForgotPasswordForm/ForgotPasswordForm.tsx
+++ apps/web/src/components/ForgotPasswordForm/ForgotPasswordForm.tsx
@@ -1,10 +1,13 @@
 import type { SubmitHandler } from 'react-hook-form';
 import type { z } from 'zod';
 
+import ROUTER_PATH from '@/constants/RouterPath';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from 'react-hook-form';
 
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 import { Button, Field } from 'ui';
+
 import { formSchema, INPUT_NAME, LABEL_EMAIL } from './constants';
 
 type FormSchemaType = z.infer<typeof formSchema>;
@@ -28,23 +31,41 @@ function ForgotPasswordForm({
   });
 
   return (
-    <div className="form-signup flex min-h-screen flex-col items-center justify-center">
+    <div
+      className="flex min-h-screen flex-col items-center justify-center"
+      id="form-forgot-password">
       <form className="rounded-2xl bg-white p-[25px]" onSubmit={handleSubmit(onSubmit)}>
         <div className="mb-4">
-          <h1 className="h3 mb-1">Forgot password</h1>
+          <h1 className="text-3xl font-bold dark:text-black">Forgot password</h1>
           <span>to continue</span>
         </div>
         <Field
-          className="mb-2"
+          className="_:mb-2"
           id="floatingInput"
           label={LABEL_EMAIL}
+          name={INPUT_NAME.EMAIL}
           type="email"
-          {...register(INPUT_NAME.EMAIL)}
+          {...{ errors, register }}
           required
         />
         <Button className="w-full" disabled={isSubmitting} type="submit" variant="primary">
           Submit
         </Button>
+        <div className="c-action gab-1 mt-3 flex flex-nowrap justify-start">
+          <span className="m-0 box-border text-sm font-normal leading-tight">
+            Have an account ?
+          </span>
+          <Link
+            className="mx-1 box-border inline-flex cursor-pointer items-center text-sm font-normal leading-tight text-gray-950 no-underline hover:text-gray-600"
+            to={ROUTER_PATH.SIGNIN}>
+            Signin
+          </Link>
+          <Link
+            className="ml-0 box-border inline-flex cursor-pointer items-center text-sm font-normal leading-tight text-gray-950 no-underline hover:text-gray-600"
+            to={ROUTER_PATH.SIGNUP}>
+            Signup
+          </Link>
+        </div>
       </form>
     </div>
   );
