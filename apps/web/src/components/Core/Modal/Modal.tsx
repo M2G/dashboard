a1f1e617e561624bd74b8828d71f61e6d119a3f5
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2023-08-16 02:55:08 +0200

    feat: update css to tailwind (wip)

--- apps/web/src/components/Core/Modal/Modal.tsx
+++ apps/web/src/components/Core/Modal/Modal.tsx
@@ -4,8 +4,6 @@ import { useEffect, useRef } from 'react';
 import Portal from '@/components/Core/Portal';
 import { Button } from 'ui';
 
-import styles from './Modal.module.scss';
-
 interface IModal {
   children: any;
   hide: () => MouseEventHandler<HTMLButtonElement>;
@@ -32,13 +30,15 @@ function Modal({ children, hide, id, isShowing, onConfirm, title }: IModal): JSX
   return isShowing ? (
     <Portal id={id}>
       <div className="fixed left-0 top-0 z-10 h-screen w-screen bg-black opacity-50">
-        <div className="z-11 fixed left-0 top-0 flex h-full w-full items-center overflow-y-auto overflow-x-hidden outline-none">
-          <div className="relative m-auto rounded-lg p-4 text-gray-900" ref={ref}>
-            <div className={styles.header}>
-              <h5 className={styles.title}>{title}</h5>
+        <div className="fixed left-0 top-0 z-20 flex h-full w-full items-center overflow-y-auto overflow-x-hidden outline-none">
+          <div
+            className="relative m-auto max-w-[400px] rounded-lg bg-[linear-gradient(to_top_right,rgba(39,39,42,1),rgba(24,24,27,1))] p-4 text-gray-900 shadow-[0_0.5rem_1rem_rgba(0,0,0,0.15)]"
+            ref={ref}>
+            <div className="relative flex items-center justify-between">
+              <h5 className="mb-2 text-white">{title}</h5>
               <button
                 aria-label="Close"
-                className={styles.close}
+                className="absolute bottom-2 left-[calc(100%-20px)]"
                 data-bs-dismiss="modal"
                 onClick={hide}
                 type="button">
@@ -58,7 +58,7 @@ function Modal({ children, hide, id, isShowing, onConfirm, title }: IModal): JSX
                 </svg>
               </button>
             </div>
-            <div className={styles.body}>{children}</div>
+            <div className="mb-2 text-white">{children}</div>
             <div className="modal-footer border-top-0 flex justify-around">
               <Button
                 className="_:bg-white _:font-normal _:text-black"
