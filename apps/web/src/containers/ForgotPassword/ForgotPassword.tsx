4d4bb4657fff402652177d2dd0a258811998ab1b
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2023-08-20 19:26:22 +0200

    feat: code refactor (perf)

--- apps/web/src/containers/ForgotPassword/ForgotPassword.tsx
+++ apps/web/src/containers/ForgotPassword/ForgotPassword.tsx
@@ -7,7 +7,7 @@ import { INITIAL_VALUES } from './constants';
 
 function ForgotPassword(): JSX.Element {
   const dispatch = useDispatch();
-  const onSubmit = useCallback((e) => dispatch(authForgotPasswordAction(e)), [dispatch]);
+  const onSubmit = useCallback((e: any) => dispatch(authForgotPasswordAction(e)), [dispatch]);
 
   return <ForgotPasswordView initialValues={INITIAL_VALUES} onSubmit={onSubmit} />;
 }
