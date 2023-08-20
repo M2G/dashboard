8a2ddb844c7f33210463dd2f54005a50fd400b29
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2023-08-20 19:24:41 +0200

    feat: create constant about form

--- apps/web/src/components/ChangePassordForm/constants.ts
+++ apps/web/src/components/ChangePassordForm/constants.ts
@@ -1,20 +1,21 @@
+import { z } from 'zod';
 export const INPUT_NAME = {
   OLD_PASSWORD: 'old_password',
   PASSWORD: 'password',
-  PASSWORD2: 'password_again'
+  PASSWORD2: 'password_again',
 };
 
 export const INITIAL_VALUES = {
   [INPUT_NAME.OLD_PASSWORD]: '',
   [INPUT_NAME.PASSWORD]: '',
-  [INPUT_NAME.PASSWORD2]: ''
+  [INPUT_NAME.PASSWORD2]: '',
 };
 
 export const ERROR_TEXT_REQUIRED = {
   ERROR_TEXT_REQUIRED_OLD_PASSWORD: 'Mot de passe requis',
   ERROR_TEXT_REQUIRED_PASSWORD: 'Mot de passe requis',
   ERROR_TEXT_REQUIRED_PASSWORD2: 'Mot de passe 2 requis',
-  ERROR_TEXT_REQUIRED_MATCH: 'Mot de passe 1 doit être égal au Mot de passe 2'
+  ERROR_TEXT_REQUIRED_MATCH: 'Mot de passe 1 doit être égal au Mot de passe 2',
 };
 
 export const LABEL_OLD_PASSWORD = 'Old Password';
@@ -24,3 +25,13 @@ export const LABEL_PASSWORD2 = 'Password';
 export const PLACEHOLDER_OLD_PASSWORD = 'Old Mot de passe';
 export const PLACEHOLDER_PASSWORD = 'Mot de passe';
 export const PLACEHOLDER_PASSWORD2 = 'Mot de passe 2';
+
+export const formSchema = z.object({
+  [INPUT_NAME.OLD_PASSWORD]: z
+    .string()
+    .email('Invalid email')
+    .min(1, ERROR_TEXT_REQUIRED.ERROR_TEXT_REQUIRED_OLD_PASSWORD),
+  [INPUT_NAME.PASSWORD]: z.string().min(1, ERROR_TEXT_REQUIRED.ERROR_TEXT_REQUIRED_OLD_PASSWORD),
+  [INPUT_NAME.PASSWORD2]: z.string().min(1, ERROR_TEXT_REQUIRED.ERROR_TEXT_REQUIRED_PASSWORD2),
+  // .min(8, 'Password must have more than 8 characters'),
+});
