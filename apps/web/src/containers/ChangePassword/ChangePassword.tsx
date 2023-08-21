a37436fdfad7a7e8827d3f2944924633651aaa7e
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2023-08-21 02:56:07 +0200

    feat: code refactor

--- apps/web/src/containers/ChangePassword/ChangePassword.tsx
+++ apps/web/src/containers/ChangePassword/ChangePassword.tsx
@@ -1,37 +1,15 @@
+import { AuthContext } from '@/AuthContext';
 import ChangePassordForm from '@/components/ChangePassordForm';
+import { useCallback, useContext } from 'react';
 
 function ChangePassword(): JSX.Element {
-  return <ChangePassordForm />;
-  /*
-const {
-  userData: { _id: id },
-} = useContext(AuthContext);
-const [changePassword] = useUpdateUserMutation();
-const handleSubmit = useCallback(
-  async (formData: {
-    id: string;
-    old_password: string;
-    password: string;
-    password_again: string;
-  }) => {
-    await changePassword({
-      variables: {
-        // eslint-disable-next-line
-        // @ts-ignore
-        id,
-        ...formData,
-      } as any,
-    });
-  },
-  [changePassword, id],
-);
+  const {
+    userData: { id: id },
+  } = useContext(AuthContext);
+  // const [changePassword] = useUpdateUserMutation();
+  const handleSubmit = useCallback(async () => {}, []);
 
-return (
-  <ChangePasswordForm
-    initialValues={INITIAL_VALUES}
-    onSubmit={handleSubmit}
-  />
-);*/
+  return <ChangePassordForm initialValues={{}} onSubmit={handleSubmit} />;
 }
 
 export default ChangePassword;
