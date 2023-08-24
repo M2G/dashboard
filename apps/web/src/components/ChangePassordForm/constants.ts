a2a96d0c79e1ff94ab0157f1158cfc0c49fe2cd0
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2023-08-24 02:49:20 +0200

    feat: add validation

--- apps/web/src/components/ChangePassordForm/constants.ts
+++ apps/web/src/components/ChangePassordForm/constants.ts
@@ -1,4 +1,5 @@
 import { z } from 'zod';
+
 export const INPUT_NAME = {
   OLD_PASSWORD: 'old_password',
   PASSWORD: 'password',
@@ -12,10 +13,10 @@ export const INITIAL_VALUES = {
 };
 
 export const ERROR_TEXT_REQUIRED = {
+  ERROR_TEXT_REQUIRED_MATCH: 'Mot de passe 1 doit être égal au Mot de passe 2',
   ERROR_TEXT_REQUIRED_OLD_PASSWORD: 'Mot de passe requis',
   ERROR_TEXT_REQUIRED_PASSWORD: 'Mot de passe requis',
   ERROR_TEXT_REQUIRED_PASSWORD2: 'Mot de passe 2 requis',
-  ERROR_TEXT_REQUIRED_MATCH: 'Mot de passe 1 doit être égal au Mot de passe 2',
 };
 
 export const LABEL_OLD_PASSWORD = 'Old Password';
@@ -31,7 +32,13 @@ export const formSchema = z.object({
     .string()
     .email('Invalid email')
     .min(1, ERROR_TEXT_REQUIRED.ERROR_TEXT_REQUIRED_OLD_PASSWORD),
-  [INPUT_NAME.PASSWORD]: z.string().min(1, ERROR_TEXT_REQUIRED.ERROR_TEXT_REQUIRED_OLD_PASSWORD),
+  [INPUT_NAME.PASSWORD]: z
+    .string()
+    .min(1, ERROR_TEXT_REQUIRED.ERROR_TEXT_REQUIRED_PASSWORD)
+    .refine((data) => data[INPUT_NAME.PASSWORD] === data[INPUT_NAME.PASSWORD2], {
+      message: ERROR_TEXT_REQUIRED.ERROR_TEXT_REQUIRED_MATCH,
+      path: [INPUT_NAME.PASSWORD],
+    }),
   [INPUT_NAME.PASSWORD2]: z.string().min(1, ERROR_TEXT_REQUIRED.ERROR_TEXT_REQUIRED_PASSWORD2),
   // .min(8, 'Password must have more than 8 characters'),
 });
