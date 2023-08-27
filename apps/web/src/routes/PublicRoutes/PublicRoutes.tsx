a4361b491fd224a570990d4e92b3ba6256a7c9af
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2023-08-27 05:38:06 +0200

    feat: code refactor (alias)

--- apps/web/src/routes/PublicRoutes/PublicRoutes.tsx
+++ apps/web/src/routes/PublicRoutes/PublicRoutes.tsx
@@ -1,13 +1,13 @@
 import type { JSX } from 'react';
 
+import ROUTER_PATH from '@/constants/RouterPath';
 import { lazy } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
-import ROUTER_PATH from 'constants/RouterPath';
 
-const Signin = lazy(() => import('../../containers/Signin'));
-const Signup = lazy(() => import('../../containers/Signup'));
-const ForgotPassword = lazy(() => import('../../containers/ForgotPassword'));
-const ResetPassword = lazy(() => import('../../containers/ResetPassword'));
+const Signin = lazy(() => import('@/containers/Signin'));
+const Signup = lazy(() => import('@/containers/Signup'));
+const ForgotPassword = lazy(() => import('@/containers/ForgotPassword'));
+const ResetPassword = lazy(() => import('@/containers/ResetPassword'));
 
 function PublicRoutes(): JSX.Element {
   return (
