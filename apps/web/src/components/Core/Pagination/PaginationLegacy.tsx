f5aa037e1845449ccdd6881f59191824399b3078
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2023-08-27 05:36:41 +0200

    feat: update pagination and rm useless code part

--- apps/web/src/components/Core/Pagination/PaginationLegacy.tsx
+++ apps/web/src/components/Core/Pagination/PaginationLegacy.tsx
@@ -36,21 +36,40 @@ function Pagination({ currentPage, perPage, setCurrentPage, totalItems }: IPagin
   return (
     <nav aria-label="-1">
       {pageNumbers.length > 1 && (
-        <ul className="pagination">
-          <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
-            <a className="page-link" href="#" onClick={handlePrevClick}>
+        <ul className="inline-flex h-10 -space-x-px text-base">
+          <li>
+            <a
+              className={`ml-0 flex h-10 items-center justify-center rounded-l-lg border border-gray-300 bg-white px-4 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white ${
+                currentPage === 1 ? 'pointer-events-none' : ''
+              }`}
+              href="#"
+              onClick={handlePrevClick}>
               Prev
             </a>
           </li>
           {displayNumbers?.map((number) => (
-            <li className={`page-item ${currentPage === number ? 'active' : ''}`} key={number}>
-              <a className="page-link" data-id={number} href="#" onClick={handleClick as any}>
+            <li>
+              <a
+                className={`flex h-10 items-center justify-center border border-gray-300 bg-white px-4 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white ${
+                  currentPage === number
+                    ? 'flex h-10 items-center justify-center border border-gray-300 bg-blue-50 px-4 text-blue-600 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white'
+                    : ''
+                }`}
+                key={number}
+                data-id={number}
+                href="#"
+                onClick={handleClick as any}>
                 {number}
               </a>
             </li>
           ))}
-          <li className={`page-item ${currentPage === pageNumbers.length ? 'disabled' : ''}`}>
-            <a className="page-link" href="#" onClick={handleNextClick}>
+          <li>
+            <a
+              className={`flex h-10 items-center justify-center rounded-r-lg border border-gray-300 bg-white px-4 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white ${
+                currentPage === pageNumbers.length ? 'pointer-events-none' : ''
+              }`}
+              href="#"
+              onClick={handleNextClick}>
               Next
             </a>
           </li>
