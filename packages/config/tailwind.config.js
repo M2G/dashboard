4f6bf612e69512d312176b8af5cd859c7e6ef965
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2023-08-28 03:11:24 +0200

    feat: add rule color

--- packages/config/tailwind.config.js
+++ packages/config/tailwind.config.js
@@ -12,6 +12,32 @@ module.exports = {
       _: "0px", // Force property priority by media query
       ...defaultTheme.screens,
     },
+    colors: {
+      transparent: 'transparent',
+      'semi-10': 'hsla(0, 0%, 0%, 0.1)',
+      'semi-20': 'hsla(0, 0%, 0%, 0.2)',
+      'semi-25': 'hsla(0, 0%, 0%, 0.25)',
+      'semi-30': 'hsla(0, 0%, 0%, 0.3)',
+      'semi-40': 'hsla(0, 0%, 0%, 0.4)',
+      'semi-50': 'hsla(0, 0%, 0%, 0.5)',
+      'semi-60': 'hsla(0, 0%, 0%, 0.6)',
+      'semi-70': 'hsla(0, 0%, 0%, 0.7)',
+      'semi-75': 'hsla(0, 0%, 0%, 0.75)',
+      'semi-80': 'hsla(0, 0%, 0%, 0.8)',
+      'semi-90': 'hsla(0, 0%, 0%, 0.9)',
+      'semi-95': 'hsla(0, 0%, 0%, 0.95)',
+
+      'semi-10-contrast': 'hsla(0, 0%, 100%, 0.1)',
+      'l1': 'hsl(0, 0%, 90%)',
+      'l2': 'hsl(0, 0%, 80%)',
+      'l3': 'hsl(0, 0%, 70%)',
+      'l4': 'hsl(0, 0%, 60%)',
+      'l5': 'hsl(0, 0%, 50%)',
+      'l6': 'hsl(0, 0%, 40%)',
+      'l7': 'hsl(0, 0%, 30%)',
+      'l8': 'hsl(0, 0%, 20%)',
+      'l9': 'hsl(0, 0%, 10%)',
+    },
     extend: {
       fontFamily: {
         display: [...defaultTheme.fontFamily.sans],
@@ -42,12 +68,6 @@ module.exports = {
           DEFAULT: "#000000",
           // active: "#D4314C",
           // disabled: "#F69BA9",
-        },
-        secondary: {
-          hover: "#F5F5F5",
-          DEFAULT: "#EBEBEB",
-          active: "#D6D6D6",
-          disabled: "#F4F4F4",
         },
         // States
         danger: {
