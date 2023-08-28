36e76b68faf51e5f87645ae6094f37c3085be07a
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2023-08-28 03:11:01 +0200

    feat: code refactor (tailwind)

--- apps/web/src/components/Core/Pagination/PaginationLegacy.tsx
+++ apps/web/src/components/Core/Pagination/PaginationLegacy.tsx
@@ -39,7 +39,7 @@ function Pagination({ currentPage, perPage, setCurrentPage, totalItems }: IPagin
         <ul className="inline-flex h-10 -space-x-px text-base">
           <li>
             <a
-              className={`ml-0 flex h-10 items-center justify-center rounded-l-lg border border-gray-300 bg-white px-4 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white ${
+              className={`border-semi-10-contrast ml-0 flex h-10 items-center justify-center rounded-l-lg border px-4 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white ${
                 currentPage === 1 ? 'pointer-events-none' : ''
               }`}
               href="#"
@@ -50,22 +50,22 @@ function Pagination({ currentPage, perPage, setCurrentPage, totalItems }: IPagin
           {displayNumbers?.map((number) => (
             <li>
               <a
-                className={`flex h-10 items-center justify-center border border-gray-300 bg-white px-4 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white ${
+                className={`border-semi-10-contrast flex h-10 items-center justify-center border px-4 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white ${
                   currentPage === number
-                    ? 'flex h-10 items-center justify-center border border-gray-300 bg-blue-50 px-4 text-blue-600 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white'
+                    ? 'flex h-10 items-center justify-center border px-4 text-blue-600 dark:text-white'
                     : ''
                 }`}
                 key={number}
                 data-id={number}
                 href="#"
-                onClick={handleClick as any}>
+                onClick={handleClick}>
                 {number}
               </a>
             </li>
           ))}
           <li>
             <a
-              className={`flex h-10 items-center justify-center rounded-r-lg border border-gray-300 bg-white px-4 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white ${
+              className={`border-semi-10-contrast flex h-10 items-center justify-center rounded-r-lg border px-4 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white ${
                 currentPage === pageNumbers.length ? 'pointer-events-none' : ''
               }`}
               href="#"
