53d1cdb49d3b452c446e9a207da06d52c647ad2f
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2023-09-03 05:29:34 +0200

    feat: code refactor (tailwind)

--- apps/web/src/components/Core/Table/TableHeaderCell.tsx
+++ apps/web/src/components/Core/Table/TableHeaderCell.tsx
@@ -8,7 +8,12 @@ interface ITableHeaderCell {
   onSort: any;
 }
 
-function TableHeaderCell({ currentSortedData, isSortable, label, onSort }: ITableHeaderCell) {
+function TableHeaderCell({
+  currentSortedData,
+  isSortable,
+  label,
+  onSort,
+}: ITableHeaderCell): JSX.Element {
   const onSortClick = () =>
     onSort(
       !currentSortedData || currentSortedData.direction === 'ascending'
@@ -20,12 +25,14 @@ function TableHeaderCell({ currentSortedData, isSortable, label, onSort }: ITabl
     currentSortedData?.direction === styles.ascending ? styles.ascending : styles.descending;
 
   return (
-    <th className={styles.th}>
+    <th className="border-b-0 p-2 pl-0 text-base font-bold">
       {label}
       {isSortable && (
         <button
           onClick={onSortClick}
-          className={clsx(styles.button, `sort-icon ${currentSortedData ? sortedClass : ''}`)}
+          className={`sort-icon mb-0 rounded-none border-0 bg-transparent font-bold ${
+            currentSortedData ? sortedClass : ''
+          }`}
         />
       )}
     </th>
