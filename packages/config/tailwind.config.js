15b3f9e6b84697a022fe1babc9a6f99fb43cb796
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2023-09-07 03:11:24 +0200

    feat: code refactor (tailwind)

--- packages/config/tailwind.config.js
+++ packages/config/tailwind.config.js
@@ -2,19 +2,85 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
   content: [
     "../../packages/ui/**/*.{js,ts,jsx,tsx}",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    screens: {
-      _: "0px", // Force property priority by media query
-      ...defaultTheme.screens,
+  darkMode: 'class',
+  extend: {
+      boxShadow: {
+        "custom": 'rgb(0 0 0 / 20%) 0 8px 10px -5px, rgb(0 0 0 / 14%) 0px 16px 24px 2px, rgb(0 0 0 / 12%) 0px 6px 30px 5px'
+      },
+      colors: {
+        // Common
+        ...defaultTheme.colors,
+        black: {
+          DEFAULT: "#000000",
+        },
+        // States
+        danger: {
+          DEFAULT: "#EB3F45",
+          disabled: "#F59FA2",
+          surface: "#FDECEC",
+        },
+        dark: {
+          DEFAULT: "#000000",
+          active: "#181818",
+          hover: "#3A3A3A",
+        },
+        grey: {
+          DEFAULT: "#EBEBEB",
+          light: "#F5F5F5",
+          // dark: "#D6D6D6",
+        },
+        header: {
+          bg: "#161617",
+        },
+        input: {
+          bg: "#121212",
+        },
+        modal: {
+          bg: "#121212",
+        },
+        navbar: {
+          bg: "#18181B",
+        },
+        // Global
+        primary: {
+          // hover: "#111827",
+          DEFAULT: "#000000",
+          // active: "#D4314C",
+          // disabled: "#F69BA9",
+        },
+        sidebar: {
+          bg: "#121212",
+        },
+        white: {
+          DEFAULT: "#FFFFFF",
+        }
+      },
+      fontFamily: {
+        body: [...defaultTheme.fontFamily.sans],
+        display: [...defaultTheme.fontFamily.sans],
+      },
     },
+    plugins: [],
+  theme: {
     colors: {
-      transparent: 'transparent',
+      "black-dark": 'hsl(0, 0%, 7%)',
+      'grey-dark': 'hsl(240, 4%, 46%)',
+      'l1': 'hsl(0, 0%, 90%)',
+      'l2': 'hsl(0, 0%, 80%)',
+      'l3': 'hsl(0, 0%, 70%)',
+      'l4': 'hsl(0, 0%, 60%)',
+      'l5': 'hsl(0, 0%, 50%)',
+      'l6': 'hsl(0, 0%, 40%)',
+      'l7': 'hsl(0, 0%, 30%)',
+      'l8': 'hsl(0, 0%, 20%)',
+      'l9': 'hsl(0, 0%, 10%)',
       'semi-10': 'hsla(0, 0%, 0%, 0.1)',
+      'semi-10-contrast': 'hsla(0, 0%, 100%, 0.1)',
+
       'semi-20': 'hsla(0, 0%, 0%, 0.2)',
       'semi-25': 'hsla(0, 0%, 0%, 0.25)',
       'semi-30': 'hsla(0, 0%, 0%, 0.3)',
@@ -25,22 +91,29 @@ module.exports = {
       'semi-75': 'hsla(0, 0%, 0%, 0.75)',
       'semi-80': 'hsla(0, 0%, 0%, 0.8)',
       'semi-90': 'hsla(0, 0%, 0%, 0.9)',
+
       'semi-95': 'hsla(0, 0%, 0%, 0.95)',
-
-      'semi-10-contrast': 'hsla(0, 0%, 100%, 0.1)',
-      'l1': 'hsl(0, 0%, 90%)',
-      'l2': 'hsl(0, 0%, 80%)',
-      'l3': 'hsl(0, 0%, 70%)',
-      'l4': 'hsl(0, 0%, 60%)',
-      'l5': 'hsl(0, 0%, 50%)',
-      'l6': 'hsl(0, 0%, 40%)',
-      'l7': 'hsl(0, 0%, 30%)',
-      'l8': 'hsl(0, 0%, 20%)',
-      'l9': 'hsl(0, 0%, 10%)',
-
-      'grey-dark': 'hsl(240, 4%, 46%)',
-      "black-dark": 'hsl(0, 0%, 7%)',
-
+      transparent: 'transparent',
+    },
+    margin: {
+        '0': '0',
+        '1': '0.25rem',
+        '2': '0.5rem',
+        '3': '0.75rem',
+        '4': '1rem',
+        '5': '1.25rem',
+        '6': '1.5rem',
+        '8': '2rem',
+        '10': '2.5rem',
+        '12': '3rem',
+        '16': '4rem',
+        '20': '5rem',
+        '24': '6rem',
+        '32': '8rem',
+        '40': '10rem',
+        'auto': 'auto',
+        'px': '1px',
+      },
       /*
   |-----------------------------------------------------------------------------
   | Padding                                https://tailwindcss.com/docs/padding
@@ -57,8 +130,7 @@ module.exports = {
   |
   */
 
-      padding: {
-        'px': '1px',
+      negativeMargin: {
         '0': '0',
         '1': '0.25rem',
         '2': '0.5rem',
@@ -74,6 +146,7 @@ module.exports = {
         '24': '6rem',
         '32': '8rem',
         '40': '10rem',
+        'px': '1px',
       },
 
 
@@ -93,9 +166,7 @@ module.exports = {
       |
       */
 
-      margin: {
-        'auto': 'auto',
-        'px': '1px',
+      padding: {
         '0': '0',
         '1': '0.25rem',
         '2': '0.5rem',
@@ -111,6 +182,7 @@ module.exports = {
         '24': '6rem',
         '32': '8rem',
         '40': '10rem',
+        'px': '1px',
       },
 
 
@@ -130,84 +202,12 @@ module.exports = {
       |
       */
 
-      negativeMargin: {
-        'px': '1px',
-        '0': '0',
-        '1': '0.25rem',
-        '2': '0.5rem',
-        '3': '0.75rem',
-        '4': '1rem',
-        '5': '1.25rem',
-        '6': '1.5rem',
-        '8': '2rem',
-        '10': '2.5rem',
-        '12': '3rem',
-        '16': '4rem',
-        '20': '5rem',
-        '24': '6rem',
-        '32': '8rem',
-        '40': '10rem',
-      },
-
-
+      screens: {
+      _: "0px", // Force property priority by media query
+      ...defaultTheme.screens,
     },
-    extend: {
-      fontFamily: {
-        display: [...defaultTheme.fontFamily.sans],
-        body: [...defaultTheme.fontFamily.sans],
-      },
-      colors: {
-        // Common
-        ...defaultTheme.colors,
-        white: {
-          DEFAULT: "#FFFFFF",
-        },
-        black: {
-          DEFAULT: "#000000",
-        },
-        dark: {
-          DEFAULT: "#000000",
-          hover: "#3A3A3A",
-          active: "#181818",
-        },
-        grey: {
-          light: "#F5F5F5",
-          DEFAULT: "#EBEBEB",
-          // dark: "#D6D6D6",
-        },
-        // Global
-        primary: {
-          // hover: "#111827",
-          DEFAULT: "#000000",
-          // active: "#D4314C",
-          // disabled: "#F69BA9",
-        },
-        // States
-        danger: {
-          DEFAULT: "#EB3F45",
-          disabled: "#F59FA2",
-          surface: "#FDECEC",
-        },
-        header: {
-          bg: "#161617",
-        },
-        navbar: {
-          bg: "#18181B",
-        },
-        sidebar: {
-          bg: "#121212",
-        },
-        modal: {
-          bg: "#121212",
-        },
-        input: {
-          bg: "#121212",
-        }
-      },
     },
-  },
   variants: {
     extend: {},
   },
-  plugins: [],
 };
