4fce1e08532273eb6fe9e15b6d84e2a84cf23c98
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2023-09-09 18:44:53 +0200

    feat: code refactor (tailwind)

--- apps/web/src/containers/UserList/List.tsx
+++ apps/web/src/containers/UserList/List.tsx
@@ -3,25 +3,25 @@ import Pagination from '@/components/Core/Pagination/Pagination';
 import TableWrapper from '@/components/Core/Table';
 
 type ListProps = {
-  header: any;
   hasNextPage: any;
   hasPrevPage: any;
+  header: any;
   id: any;
   rows: any;
   setCurrentPage: any;
 };
 
 function List({
-  id,
-  header,
-  rows,
   hasNextPage,
   hasPrevPage,
+  header,
+  id,
+  rows,
   setCurrentPage,
 }: ListProps): JSX.Element {
   return (
     <>
-      <TableWrapper id={id} header={header} rows={rows} />
+      <TableWrapper header={header} id={id} rows={rows} />
       <div className="d-inline-flex">
         <Pagination
           hasNextPage={hasNextPage}
