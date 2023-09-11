1fe5ca859d4903dc089ffd0e4c13509854629881
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2023-09-11 02:25:39 +0200

    feat: code refactor

--- apps/web/src/containers/Profil/index.tsx
+++ apps/web/src/containers/Profil/index.tsx
@@ -1,7 +1,6 @@
 import Navbar from '@/components/Navbar/Navbar';
 import Sidebar from '@/components/Sidebar';
 import Profil from './Profil';
-import './index.scss';
 
 function ProfilPage(): JSX.Element {
   return (
