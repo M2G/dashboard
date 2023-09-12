18d79453f532bc6e571acef227be9db2d8a00e56
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2023-09-12 03:11:09 +0200

    fix: conf

--- packages/config/tailwind.config.js
+++ packages/config/tailwind.config.js
@@ -41,115 +41,113 @@ module.exports = {
       'grey-dark': 'hsl(240, 4%, 46%)',
       "black-dark": 'hsl(0, 0%, 7%)',
 
-      /*
-  |-----------------------------------------------------------------------------
-  | Padding                                https://tailwindcss.com/docs/padding
-  |-----------------------------------------------------------------------------
-  |
-  | Here is where you define your padding utility sizes. These can be
-  | percentage based, pixels, rems, or any other units. By default we
-  | provide a sensible rem based numeric scale plus a couple other
-  | common use-cases like "1px". You can, of course, modify these
-  | values as needed.
-  |
-  | Class name: .p{side?}-{size}
-  | CSS property: padding
-  |
-  */
+    },
+    /*
+|-----------------------------------------------------------------------------
+| Padding                                https://tailwindcss.com/docs/padding
+|-----------------------------------------------------------------------------
+|
+| Here is where you define your padding utility sizes. These can be
+| percentage based, pixels, rems, or any other units. By default we
+| provide a sensible rem based numeric scale plus a couple other
+| common use-cases like "1px". You can, of course, modify these
+| values as needed.
+|
+| Class name: .p{side?}-{size}
+| CSS property: padding
+|
+*/
 
-      padding: {
-        'px': '1px',
-        '0': '0',
-        '1': '0.25rem',
-        '2': '0.5rem',
-        '3': '0.75rem',
-        '4': '1rem',
-        '5': '1.25rem',
-        '6': '1.5rem',
-        '8': '2rem',
-        '10': '2.5rem',
-        '12': '3rem',
-        '16': '4rem',
-        '20': '5rem',
-        '24': '6rem',
-        '32': '8rem',
-        '40': '10rem',
-      },
-
-
-      /*
-      |-----------------------------------------------------------------------------
-      | Margin                                  https://tailwindcss.com/docs/margin
-      |-----------------------------------------------------------------------------
-      |
-      | Here is where you define your margin utility sizes. These can be
-      | percentage based, pixels, rems, or any other units. By default we
-      | provide a sensible rem based numeric scale plus a couple other
-      | common use-cases like "1px". You can, of course, modify these
-      | values as needed.
-      |
-      | Class name: .m{side?}-{size}
-      | CSS property: margin
-      |
-      */
-
-      margin: {
-        'auto': 'auto',
-        'px': '1px',
-        '0': '0',
-        '1': '0.25rem',
-        '2': '0.5rem',
-        '3': '0.75rem',
-        '4': '1rem',
-        '5': '1.25rem',
-        '6': '1.5rem',
-        '8': '2rem',
-        '10': '2.5rem',
-        '12': '3rem',
-        '16': '4rem',
-        '20': '5rem',
-        '24': '6rem',
-        '32': '8rem',
-        '40': '10rem',
-      },
+    padding: {
+      'px': '1px',
+      '0': '0',
+      '1': '0.25rem',
+      '2': '0.5rem',
+      '3': '0.75rem',
+      '4': '1rem',
+      '5': '1.25rem',
+      '6': '1.5rem',
+      '8': '2rem',
+      '10': '2.5rem',
+      '12': '3rem',
+      '16': '4rem',
+      '20': '5rem',
+      '24': '6rem',
+      '32': '8rem',
+      '40': '10rem',
+    },
 
 
-      /*
-      |-----------------------------------------------------------------------------
-      | Negative margin                https://tailwindcss.com/docs/negative-margin
-      |-----------------------------------------------------------------------------
-      |
-      | Here is where you define your negative margin utility sizes. These can
-      | be percentage based, pixels, rems, or any other units. By default we
-      | provide matching values to the padding scale since these utilities
-      | generally get used together. You can, of course, modify these
-      | values as needed.
-      |
-      | Class name: .-m{side?}-{size}
-      | CSS property: margin
-      |
-      */
+    /*
+    |-----------------------------------------------------------------------------
+    | Margin                                  https://tailwindcss.com/docs/margin
+    |-----------------------------------------------------------------------------
+    |
+    | Here is where you define your margin utility sizes. These can be
+    | percentage based, pixels, rems, or any other units. By default we
+    | provide a sensible rem based numeric scale plus a couple other
+    | common use-cases like "1px". You can, of course, modify these
+    | values as needed.
+    |
+    | Class name: .m{side?}-{size}
+    | CSS property: margin
+    |
+    */
 
-      negativeMargin: {
-        'px': '1px',
-        '0': '0',
-        '1': '0.25rem',
-        '2': '0.5rem',
-        '3': '0.75rem',
-        '4': '1rem',
-        '5': '1.25rem',
-        '6': '1.5rem',
-        '8': '2rem',
-        '10': '2.5rem',
-        '12': '3rem',
-        '16': '4rem',
-        '20': '5rem',
-        '24': '6rem',
-        '32': '8rem',
-        '40': '10rem',
-      },
+    margin: {
+      'auto': 'auto',
+      'px': '1px',
+      '0': '0',
+      '1': '0.25rem',
+      '2': '0.5rem',
+      '3': '0.75rem',
+      '4': '1rem',
+      '5': '1.25rem',
+      '6': '1.5rem',
+      '8': '2rem',
+      '10': '2.5rem',
+      '12': '3rem',
+      '16': '4rem',
+      '20': '5rem',
+      '24': '6rem',
+      '32': '8rem',
+      '40': '10rem',
+    },
 
 
+    /*
+    |-----------------------------------------------------------------------------
+    | Negative margin                https://tailwindcss.com/docs/negative-margin
+    |-----------------------------------------------------------------------------
+    |
+    | Here is where you define your negative margin utility sizes. These can
+    | be percentage based, pixels, rems, or any other units. By default we
+    | provide matching values to the padding scale since these utilities
+    | generally get used together. You can, of course, modify these
+    | values as needed.
+    |
+    | Class name: .-m{side?}-{size}
+    | CSS property: margin
+    |
+    */
+
+    negativeMargin: {
+      'px': '1px',
+      '0': '0',
+      '1': '0.25rem',
+      '2': '0.5rem',
+      '3': '0.75rem',
+      '4': '1rem',
+      '5': '1.25rem',
+      '6': '1.5rem',
+      '8': '2rem',
+      '10': '2.5rem',
+      '12': '3rem',
+      '16': '4rem',
+      '20': '5rem',
+      '24': '6rem',
+      '32': '8rem',
+      '40': '10rem',
     },
     extend: {
       fontFamily: {
