a3ca53a64b9f36c3d9f87dabcf41a549997fc46a
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2023-09-15 03:23:31 +0200

    feat: code refactor (tailwind)

--- apps/web/src/containers/UserList/Action/AddUser.tsx
+++ apps/web/src/containers/UserList/Action/AddUser.tsx
@@ -1,22 +1,19 @@
 import { useTranslation } from 'react-i18next';
 
 interface IAddUser {
-  canAdd: boolean;
   onAdd: () => void;
 }
 
-function AddUser({ canAdd, onAdd }: IAddUser): JSX.Element | null {
+function AddUser({ onAdd }: IAddUser): JSX.Element | null {
   const { t } = useTranslation();
   return (
     <div className="flex w-full justify-end">
-      {canAdd && (
-        <button
-          className="m-4 mb-2 rounded-md px-4 py-2 text-sm font-medium text-gray-800 hover:bg-gray-200 dark:bg-white"
-          onClick={onAdd}
-          type="submit">
-          {t('Add user')}
-        </button>
-      )}
+      <button
+        className="m-4 mb-2 rounded-md px-4 py-2 text-sm font-medium text-gray-800 hover:bg-gray-200 dark:bg-white"
+        onClick={onAdd}
+        type="submit">
+        {t('Add user')}
+      </button>
     </div>
   );
 }
