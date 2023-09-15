d7f25bcfd97412e02e248595c4b4eb7128072e88
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2023-09-15 03:23:42 +0200

    feat: code refactor (tailwind)

--- apps/web/src/containers/UserList/ListLegacy.tsx
+++ apps/web/src/containers/UserList/ListLegacy.tsx
@@ -4,6 +4,18 @@ import PageSize from '@/components/Core/Pagination/PageSize';
 import Pagination from '@/components/Core/Pagination/PaginationLegacy';
 import TableWrapper from '@/components/Core/Table';
 
+interface ListProps {
+  count: number;
+  currentPageSize: number;
+  currentPage: number;
+  data: any;
+  header: any;
+  id: string;
+  rows: any;
+  setCurrentPage: (page: number) => void;
+  setCurrentPageSize: (pageSize: number) => void;
+}
+
 function List({
   count,
   currentPage,
@@ -14,12 +26,12 @@ function List({
   rows,
   setCurrentPage,
   setCurrentPageSize,
-}: any): JSX.Element {
+}: ListProps): JSX.Element {
   const perPage = currentPageSize;
   return (
     <>
       <TableWrapper header={header} id={id} rows={rows} />
-      <div className="w-100 mt-2 flex items-center justify-end">
+      <div className="w-100 flex items-center justify-end">
         <PageSize currentPageSize={currentPageSize} setCurrentPageSize={setCurrentPageSize} />
         <Pagination
           currentPage={currentPage}
