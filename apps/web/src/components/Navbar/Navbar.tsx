d9b6b9b2cab0e6d02cc584d2a4fa11b7a504e754
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2023-09-23 05:21:53 +0200

    feat: add i18n

--- apps/web/src/components/Navbar/Navbar.tsx
+++ apps/web/src/components/Navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 import type { JSX } from 'react';
 
-import { useCallback, useState } from 'react';
+import { useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
 
 enum Language {
