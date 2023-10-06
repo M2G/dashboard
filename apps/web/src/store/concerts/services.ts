aea2e59c3f9771ee9801c0f37fde4a3a3c146794
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2023-10-06 02:52:09 +0200

    feat: rewrite service for concert (wip)

--- apps/web/src/store/concerts/services.ts
+++ apps/web/src/store/concerts/services.ts
@@ -1,24 +1,8 @@
 /* eslint-disable */
 import api from 'api';
 
-function forgotPasswordService(params: any): Promise<any> {
-  return api.post('/auth/forgot-password', params);
-}
-
-function recoverPasswordService(params: any): Promise<any> {
-  return api.post('/auth/reset-password', params);
-}
-
 function userProfilService(id: string): Promise<any> {
   return api.get(`/auth/users/${id}`);
-}
-
-function createUserProfilService(params: any): Promise<any> {
-  return api.post(`/users`, params);
-}
-
-function updateUserProfilService({ id, ...params }: any): Promise<any> {
-  return api.put(`/auth/users/${id}`, params);
 }
 
 function getUsersService({ filters, page, pageSize }): Promise<any> {
@@ -31,16 +15,4 @@ function getUsersService({ filters, page, pageSize }): Promise<any> {
   );
 }
 
-function deleteUsersService(id: string): Promise<any> {
-  return api.delete(`/auth/users/${id}`);
-}
-
-export {
-  forgotPasswordService,
-  recoverPasswordService,
-  getUsersService,
-  createUserProfilService,
-  userProfilService,
-  updateUserProfilService,
-  deleteUsersService,
-};
+export { getUsersService, userProfilService };
